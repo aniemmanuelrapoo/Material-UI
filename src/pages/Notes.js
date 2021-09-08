c620556781b6faae50a6015a25c9157189d01992
Emmanuel rapoo <aniemmanuelrapo@gmail.com>
2021-09-07 19:15:35 -0700

    commited all files first

---
+++ src/pages/Notes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Notes() {
+    return (
+        <div>
+            <h1>this is notes</h1>
+            <h1>this is notes</h1>
+        </div>
+    )
+}
+
